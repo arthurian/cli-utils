3f0cc2f13f71f44d336f1611723bcdf61ea576f2
Arthur Barrett <abarrett@fas.harvard.edu>
2017-08-18 11:27:24 -0400

    Added CLI to pull out aws credentials.

---
+++ bin/awscreds.py
@@ -0,0 +1,25 @@
+# vim: set ft=python noai et ts=4 sw=4:
+
+import os
+import os.path
+import sys
+import ConfigParser
+
+credentials_file = os.path.join(os.path.expanduser('~'), '.aws', 'credentials')
+items = {'key': 'aws_access_key_id', 'secret': 'aws_secret_access_key'}
+
+if len(sys.argv) < 2:
+    print "Usage: %s profileName [key|secret]" % sys.argv[0]
+    sys.exit(0)
+
+config = ConfigParser.RawConfigParser()
+config.read(credentials_file)
+section = sys.argv[1]
+if len(sys.argv) == 2:
+    print "\n".join([config.get(section, items[item]) for item in ('key', 'secret')])
+else:
+    if sys.argv[2] in items:
+        print config.get(section, items[sys.argv[2]])
+    else:
+        sys.stderr.write("Invalid credential item specified: %s\n" % sys.argv[2])
+        sys.exit(1)
